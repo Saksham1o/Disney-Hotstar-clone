c3c580184658a5f720fefdcf692b984e3111d2c6
Saksham1o <sakshampatiyar26@gmail.com>
2024-07-23 22:33:31 +0530

    adding style

---
+++ script.js
@@ -0,0 +1,29 @@
+let movies = [
+    {
+      name: "loki",
+      des: "Loki is an American television series created by Michael Waldron for the streaming service Disney.",
+      image: "./images/slider 1.PNG",
+    },
+    {
+      name: "falcon and the winter soldier",
+      des: "Falcon and the Winter Soldier is an American television series created for the streaming platform Disney+.",
+      image: "./images/slider 2.PNG",
+    },
+    {
+      name: "WandaVision",
+      des: "WandaVision is an American television series created for the streaming service Disney+",
+      image: "./images/slider 3.PNG",
+    },
+  
+    {
+      name: "Raya and the Last Dragon",
+      des: "Raya and the Last Dragon is an animated Disney film released in 2021.",
+      image: "./images/slider 4.PNG",
+    },
+  
+    {
+      name: "Luca",
+      des: "Luca is a Disney-Pixar animated film released in 2021.",
+      image: "./images/slider 5.PNG",
+    },
+];
