14a561fc76020f6c68e835ec0b428d35cdee1e47
Saksham1o <sakshampatiyar26@gmail.com>
2024-07-24 02:44:55 +0530

    done

--- script.js
+++ script.js
@@ -108,3 +108,20 @@ function scrollLeft(container) {
   container.scrollLeft -= container.clientWidth;
 }
 
+// Function to scroll the card container to the right
+function scrollRight(container) {
+    container.scrollLeft += container.clientWidth;
+  }
+  
+// Attach click event listeners to previous and next buttons
+preBtns.forEach((preBtn, index) => {
+    preBtn.addEventListener("click", () => {
+      scrollLeft(cardContainers[index]);
+    });
+});
+  
+nxtBtns.forEach((nxtBtn, index) => {
+    nxtBtn.addEventListener("click", () => {
+      scrollRight(cardContainers[index]);
+    });
+});
